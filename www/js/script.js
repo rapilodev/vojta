c76c3940b5f4ce035719459b35cc2bb37a30d94a
Milan <mc@radiopiloten.de>
2018-09-09 21:17:08 +0200

    add icon and global clock

--- www/js/script.js
+++ www/js/script.js
@@ -1,76 +1,69 @@
-var clockUpdate;
-var round=0;
+var clockTimer = null;
+var totalTimer = null;
+var round = 0;
 
-function setRound(value){
-    round=value;
+function setRound(value) {
+	round = value;
 	var roundElem = document.getElementById("round")
-    roundElem.innerHTML= "Runde "+round;
+	roundElem.innerHTML = "Runde " + round;
 }
 
-function increaseRound(){
-    stopClock();
-    setRound(round+1);
-    initClock();
-    startClock();
+function showClock(id, minute, second) {
+	if (minute < 10)
+		minute = "0" + minute;
+	if (second < 10)
+		second = "0" + second;
+	var clockElem = document.getElementById(id);
+	clockElem.innerHTML = minute + ":" + second;
 }
 
-function setClock(minute, second){
-    if (minute<10) minute="0"+minute;
-    if (second<10) second="0"+second ;
-	var clockElem = document.getElementById("clock");
-	clockElem.innerHTML= minute+":"+second;
+function clear(timer) {
+	if (timer == null)
+		return;
+	console.log(timer)
+	clearInterval(timer);
+	timer = null;
 }
 
-function initClock(){
-	setClock(0,0);
+function startClock(id, timer) {
+	var date = new Date();
+	var start = date.getTime() / 1000;
+
+	clear(timer);
+	timer = setInterval(function() {
+		var now = new Date().getTime() / 1000;
+		var diff = now - start;
+
+		var minute = parseInt(diff / 60);
+		var second = parseInt(diff % 60);
+		showClock(id, minute, second);
+	}, 1000);
+	return timer
 }
 
-function clearIntervals(){
-    for (var i = 1; i < 1000; i++){
-        window.clearInterval(i);
-    }
+function restartClock(id, timer) {
+	clear(timer);
+	showClock(id, 0, 0);
+	timer = startClock(id, timer);
+	return timer;
 }
 
-function startClock(){
-    var date=new Date();
-    var start=date.getTime()/1000;
-                    
-    clearIntervals();
-    clockUpdate = setInterval(
-        function(){ 
-            var now = new Date().getTime()/1000;
-            var diff=now-start;
+$(document).ready(function() {
+	showClock("clock", 0, 0);
+	setRound(1);
 
-            var minute=parseInt(diff / 60);
-            var second=parseInt(diff % 60);
-		    setClock(minute,second);
-        }, 
-        1000
-    );
-}
+	$("#total").click(function() {
+		totalTimer = restartClock("total", totalTimer)
+		clockTimer = restartClock("clock", clockTimer);
+		setRound(1);
+	});
 
-function stopClock(){
-    clearIntervals();
-}
+	$("#round").click(function() {
+		setRound(round + 1);
+		clockTimer = restartClock("clock", clockTimer);
+	});
 
-$( document ).ready(
-    function() {
-        initClock();
-        setRound(1);
-
-        $("#round").click(
-            function() {
-                increaseRound();
-            }
-        );
-
-        $("#clock").click(
-            function(){
-                stopClock();
-                initClock();
-                startClock();
-            }
-        );
-    }
-)
-
+	$("#clock").click(function() {
+		clockTimer = restartClock("clock", clockTimer);
+	});
+})
